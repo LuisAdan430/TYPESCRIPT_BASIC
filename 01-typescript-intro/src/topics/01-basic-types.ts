db9e9459e27218d1192f87f488d5b2125361933c
LuisAdan430 <luisangelcanalescalva@gmail.com>
2024-02-23 21:54:24 -0600

    principios basicos curso Angular

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,8 @@
+const name: string = 'Luis';
+let hpPoitns: number | 'FULL' = 95;
+const isAlive: boolean = true;
+hpPoitns = 'FULL';
+console.log({
+    name,hpPoitns,isAlive
+})
+export{};
---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,48 @@
+export class Person{
+    //public name: string;
+    //private address: string;
+    /*
+    constructor(name: string, address: string){
+        this.name = name;
+        this.address = address;
+    }
+    */
+   constructor(public firstName:string,public lastName:string, private address:string = 'No Address'){}
+}
+/*
+export class Hero extends Person{
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        super(realName,'New York');
+    }
+}
+*/
+/*
+export class Hero {
+    public person:Person;
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        this.person = new Person(realName);
+    }
+}
+*/
+export class Hero {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person:Person
+    ){
+        
+    }
+}
+const tony = new Person('Tony','Stark', 'New York');
+const ironman = new Hero('Hulk',45,'Bruce',tony);
+console.table(ironman);
+//console.table(person);
