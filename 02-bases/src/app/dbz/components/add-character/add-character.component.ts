b3f94eacf730f3fb278d790bee46e14fbdd87440
LuisAdan430 <luisangelcanalescalva@gmail.com>
2024-03-26 22:20:01 -0600

    Ultimos cambios

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,11 +7,28 @@ import { Character } from '../../interfaces/character.interface';
   styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
+   @Output()
+   public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+
    public character : Character = {
       name: '',
       power: 0
    };
    emitCaracter():void{
+    //debugger;
+
     console.log(this.character);
+    if(this.character.name.length === 0) return;
+    this.onNewCharacter.emit(this.character);
+
+
+    this.character = {name: '', power: 0 };
+
+
+    /*
+    this.character.name = '';
+    this.character.power = 0;
+    */
    }
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -17,4 +17,11 @@ export class MainPageComponent  {
     name: 'Vegeta',
     power: 7500
   }];
+
+  onNewCharacter(character: Character):void{
+    //console.log('MainPage');
+    //console.log(character);
+    this.characters.push(character);
+
+  }
 }
