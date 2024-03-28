c7f7140dcc3fe076d094a7e70cc8d79995f21375
LuisAdan430 <luisangelcanalescalva@gmail.com>
2024-03-27 22:28:11 -0600

    Cambios del dia dia 27/03/2024

---
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+  public characters : Character[] = [{
+    id: uuid(),
+    name: 'Krillin',
+    power: 1000
+  }, {
+    id: uuid(),
+    name: 'Goku',
+    power: 9500
+  }, {
+    id: uuid(),
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  onNewCharacter(character: Character):void{
+    //console.log('MainPage');
+    //console.log(character);
+
+    this.characters.push(character);
+
+  }
+
+  onDeleteCharacter(index:number){
+   this.characters.splice(index,1);
+  }
+
+}
